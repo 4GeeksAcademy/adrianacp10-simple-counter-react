5814d546c166793ddb9113876c8cb02a33cb949b
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-01-11 17:01:30 +0000

    final1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div class="clock">
+			<i class="fas fa-clock"></i>
+			<div class="number">
+			<h1>{props.digitFour%10}</h1>
+			<h1>{props.digitThree&10}</h1>
+			<h1>{props.digitTwo%10}</h1>
+			<h1>{props.digitOne%10}</h1>
+			</div>
+			
 		</div>
 	);
 };
 
 export default Home;
+
+//no consigo poner un icono de fontawesome y no sé como hacer que el contador funcione con el setInterval
+//el archivo clock.jsx es necesario o podría haberlo creado directamente desde home y el index.js ?
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,26 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+
+ setInterval( () => {  
+    
+    const one = Math.floor(counter/1)
+    const two = Math.floor(counter/10)
+    const three = Math.floor(counter/100)
+    const four = Math.floor(counter/1000)
+    counter++
+    
+
+
+    ReactDOM.render(<Home digitOne={one}
+                          digitTwo={two}
+                          digitThree={three}
+                          digitFour={four}
+
+    />, document.querySelector("#app"))}, 1000);
